5b8345edec79692047ab740c6ac850de512228c8
秋雨落 <i@rain.cx>
2023-03-10 08:47:03 +0800

    tada: Init.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ HitAndBlow/HitAndBlowGame.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+
+namespace HitAndBlow;
+
+public class HitAndBlowGame
+{
+    private Random Random { get; }
+    
+    public int Digits { get; }
+    
+    public string Number { get; private set; }
+
+    public bool IsEnded { get; private set; } = true;
+    public bool IsLastSuccess { get; private set; } = false;
+    public int TriedTimes { get; private set; } = 0;
+    public int RoundTriedTimes { get; private set; } = 0;
+    public int TriedRounds { get; private set; } = 0;
+    public int SuccessTimes { get; private set; } = 0;
+    
+    public HitAndBlowGame(int digits)
+    {
+        if (digits is > 10 or < 2)
+        {
+            throw new ArgumentException("Digits must less than 10 and more than 2.");
+        }
+
+        Random = new Random();
+
+        Digits = digits;
+    }
+
+    public string New()
+    {
+        IsEnded = false;
+        TriedRounds += 1;
+        RoundTriedTimes = 0;
+        
+        StringBuilder builder = new();
+        for (var i = 0; i < Digits; i++)
+        {
+            builder.Append(Random.Next(0, 10));
+        }
+
+        Number = builder.ToString();
+        return Number;
+    }
+
+    public (int Hit, int Blow) Guess(string number)
+    {
+        if (number.Length != Digits)
+        {
+            throw new ArgumentException($"Guess length mismatch with {Digits}!");
+        }
+
+        if (number.Length != number.Distinct().Count())
+        {
+            throw new ArgumentException($"Distinct length mismatched with {Digits}.");
+        }
+
+        if (IsEnded)
+        {
+            throw new InvalidOperationException("Round ended!");
+        }
+        
+        TriedTimes += 1;
+        RoundTriedTimes += 1;
+        
+        var hit = 0;
+        var blow = 0;
+
+        for (var i = 0; i < Digits; i++)
+        {
+            if (Number[i] == number[i])
+            {
+                hit += 1;
+            } 
+            else if (Number.Contains(number[i]))
+            {
+                blow += 1;
+            }
+        }
+        
+        if (hit == Digits && blow == 0)
+        {
+            SuccessTimes += 1;
+            IsLastSuccess = true;
+            IsEnded = true;
+        }
+        else
+        {
+            IsLastSuccess = false;
+        }
+        
+        return (hit, blow);
+    }
+}
---
+++ HitAndBlow/Program.cs
@@ -0,0 +1,105 @@
+﻿using HitAndBlow;
+
+HitAndBlowGame game = null;
+
+Console.WriteLine("Hello, Hit & Blow.");
+
+while (true)
+{
+    Console.WriteLine("Please key-in operations:");
+    var operation = Console.ReadLine();
+    
+    if (operation.StartsWith("new"))
+    {
+        var split = operation.Split(" ");
+        if (split.Length != 2)
+        {
+            Console.WriteLine("Invalid input.");
+            continue;
+        }
+        
+        if (int.TryParse(split[1], out var digits))
+        {
+            try
+            {
+                game = new HitAndBlowGame(digits);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        Console.WriteLine("A new game.");
+    }
+    else if (operation.StartsWith("analyze"))
+    {
+        if (game is null)
+        {
+            Console.WriteLine("Game is null!");
+            continue;
+        }
+
+        var round = game.TriedRounds;
+        var tried = game.TriedTimes;
+        var succeed = game.SuccessTimes;
+        Console.WriteLine($"{succeed} succeed of {tried} tries in {round} rounds.");
+    }
+    else if (operation.StartsWith("next"))
+    {
+        if (game is null)
+        {
+            Console.WriteLine("Game is null!");
+            continue;
+        }
+        
+        game.New();
+        Console.WriteLine("Next round.");
+    }
+    else if (operation.StartsWith("guess"))
+    {
+        if (game is null)
+        {
+            Console.WriteLine("Game is null!");
+            continue;
+        }
+
+        var split = operation.Split(" ");
+        if (split.Length != 2)
+        {
+            Console.WriteLine("Invalid input.");
+            continue;
+        }
+
+        if (game.IsEnded)
+        {
+            Console.WriteLine("Round ended.");
+        }
+        
+        try
+        {
+            var result = game.Guess(split[1]);
+
+            Console.WriteLine($"Hit/Blow: {result.Hit} {result.Blow}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+    else if (operation.StartsWith("exit"))
+    {
+        if (game is null)
+        {
+            Console.WriteLine("Game is null!");
+            continue;
+        }
+
+        var round = game.TriedRounds;
+        var tried = game.TriedTimes;
+        var succeed = game.SuccessTimes;
+        Console.WriteLine($"{succeed} succeed of {tried} tries in {round} rounds.");
+        
+        break;
+    }
+}
